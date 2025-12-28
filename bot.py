b4b560d2031c5f94514424023f1d93b46c517292
osmankerdi <Osmankerdi71@gmail.com>
2025-12-28 03:41:33 +0200

    add telegram bot code

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8288625060:AAHiB7DP7pmW6c3Nzv5fCfJjTM-Zvf8A97U"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("أهلاً! البوت شغّال ✅")
+    await update.message.reply_text("أهلا 👋 البوت شغّال!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
