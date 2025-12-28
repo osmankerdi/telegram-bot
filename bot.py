a00e91ef706375409f9d10aadcc9532f06b78129
osmankerdi <Osmankerdi71@gmail.com>
2025-12-28 03:38:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = "8288625060:AAHiB7DP7pmW6c3Nzv5fCfJjTM-Zvf8A97U"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("أهلاً! البوت شغّال ✅")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
